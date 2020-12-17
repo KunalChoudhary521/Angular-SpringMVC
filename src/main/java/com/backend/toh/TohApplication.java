bcfa35fc333b200c38fd6a5ce3a8e012630bf6c1
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-12-17 10:02:10 -0500

    Initial commit

---
+++ src/main/java/com/backend/toh/TohApplication.java
@@ -0,0 +1,13 @@
+package com.backend.toh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TohApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/toh/TohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.toh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
