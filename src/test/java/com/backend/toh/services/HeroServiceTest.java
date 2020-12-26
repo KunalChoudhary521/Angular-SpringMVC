878479858809ecc6b30a07db24d62ba6115a3c7c
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-12-26 10:41:19 -0500

    API: Use Junit 5 for unit tests

--- src/test/java/com/backend/toh/services/HeroServiceTest.java
+++ src/test/java/com/backend/toh/services/HeroServiceTest.java
@@ -3,11 +3,11 @@ package com.backend.toh.services;
 import com.backend.toh.domain.Hero;
 import com.backend.toh.entities.HeroDto;
 import com.backend.toh.repositories.HeroRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class HeroServiceTest {
 
     @Mock
