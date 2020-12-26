642dd201f897a3cf21893d4e64532affa90537ac
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2020-12-26 10:41:35 -0500

    API: Add IT smoke test

--- src/test/java/com/backend/toh/TohApplicationTests.java
+++ src/test/java/com/backend/toh/TohApplicationTests.java
@@ -1,13 +1,26 @@
 package com.backend.toh;
 
+import com.backend.toh.controllers.HeroesController;
+import com.backend.toh.controllers.UIController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TohApplicationTests {
 
+	@Autowired
+	private HeroesController heroesController;
+
+	@Autowired
+	private UIController uiController;
+
 	@Test
 	void contextLoads() {
+		assertThat(heroesController).isNotNull();
+		assertThat(uiController).isNotNull();
 	}
 
 }
